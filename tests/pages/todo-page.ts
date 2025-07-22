bbcb2411fc26d22a82abe452f3d8f489169b579b
37255 <aleksandra.kulakova.ndu@gmail.com>
2025-07-22 20:38:35 +0300

    Added remaining files

--- tests/pages/todo-page.ts
+++ tests/pages/todo-page.ts
@@ -1,7 +1,41 @@
-import {Locator} from "@playwright/test";
+import {expect, Locator, Page} from "@playwright/test";
+export class TodoPage {
+    readonly page: Page
+    readonly header: Locator
+    readonly input: Locator
+    readonly toDoText: Locator
+    readonly buttonAll: Locator
+    readonly buttonActive: Locator
+    readonly buttonCompleted: Locator
+    readonly buttonClearCompleted: Locator
+    readonly buttonDeleteX: Locator
+    readonly toggleActive:Locator
+    readonly toggleAll: Locator
 
-class  TodoPage {
-    readonly Page: Page
-    readonly Header: Locator
-    readonly 
+    constructor(page: Page) {
+        this.page = page
+        this.header = page.getByTestId('header')
+        this.input = page.getByTestId('text-input')
+        this.toDoText = page.getByTestId('todo-item-label')
+        this.buttonAll = page.getByRole('link', { name: 'All' })
+        this.buttonActive = page.getByRole('link', { name: 'Active' })
+        this.buttonCompleted = page.getByRole('link', { name: 'Completed' })
+        this.buttonClearCompleted = page.getByRole('button', { name: 'Clear completed' })
+        this.buttonDeleteX = page.getByTestId('todo-item-button')
+        this.toggleActive = page.getByTestId('todo-item-toggle')
+        this.toggleAll = page.getByTestId('toggle-all')
+
+    }
+
+    async countToDoItems(page): Promise<number> {
+        const todoItemsLabels = page.getByTestId('todo-item-label')
+        const numberOfTasks = await todoItemsLabels.count()
+        return numberOfTasks
+    }
+
+    async completeTaskByName(name: string) {
+        const task = this.page.getByRole('listitem').filter({ hasText: name });
+        await task.getByTestId('todo-item-toggle').check();
+    }
+
 }
---
+++ tests/todo-app.spec.ts
@@ -0,0 +1,111 @@
+import {expect, test} from "@playwright/test";
+import {TodoPage} from "./pages/todo-page";
+
+
+test('create a task', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+    await page.goto('https://todo-app.tallinn-learning.ee/#/');
+    await todoPage.input.fill ('test')
+    await todoPage.input.press('Enter')
+    await expect.soft(todoPage.toDoText).toHaveText('test')
+
+});
+
+test('delete a task', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+    await page.goto('https://todo-app.tallinn-learning.ee/#/');
+
+    await todoPage.input.fill ('Apple');
+    await todoPage.input.press('Enter');
+    await todoPage.input.fill ('Cake');
+    await todoPage.input.press('Enter');
+    const item = page.locator('li', { hasText: 'Cake'});
+    await item.hover();
+    await item.locator('[data-testid="todo-item-button"]').click();
+
+    const count = await todoPage.countToDoItems(page);
+    await expect.soft(count).toBe(1);
+
+});
+
+
+test('mark a task as completed by name', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+    await page.goto('https://todo-app.tallinn-learning.ee/#/');
+    await todoPage.input.fill('Apple');
+    await todoPage.input.press('Enter');
+    await todoPage.input.fill('Cake');
+    await todoPage.input.press('Enter');
+    const item = page.locator('li', { hasText: 'Apple'});
+    await item.hover();
+    await item.locator('[data-testid="todo-item-toggle"]').click();
+
+    await todoPage.buttonCompleted.click();
+    await expect.soft(page.getByText('Apple')).toBeVisible();
+    await expect.soft(page.getByText('Cake')).toHaveCount(0);
+});
+
+test('filter tasks using Active and Completed buttons', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+    await page.goto('https://todo-app.tallinn-learning.ee/#/');
+    await todoPage.input.fill('Apple');
+    await todoPage.input.press('Enter');
+    await todoPage.input.fill('Cake');
+    await todoPage.input.press('Enter');
+
+    const completedItem = page.getByRole('listitem').filter({ hasText: 'Cake' });
+    await completedItem.getByTestId('todo-item-toggle').check();
+
+    await todoPage.buttonActive.click();
+    await expect.soft(page.getByText('Apple')).toBeVisible();
+    await expect.soft(page.getByText('Cake')).toHaveCount(0);
+
+    await todoPage.buttonCompleted.click();
+    await expect.soft(page.getByText('Cake')).toBeVisible();
+    await expect.soft(page.getByText('Apple')).toHaveCount(0);
+});
+
+test('clear completed tasks', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+    await page.goto('https://todo-app.tallinn-learning.ee/#/');
+    await todoPage.input.fill('Apple');
+    await todoPage.input.press('Enter');
+    await todoPage.input.fill('Cake');
+    await todoPage.input.press('Enter');
+    await todoPage.input.fill('Icecream');
+    await todoPage.input.press('Enter');
+
+    const item = page.locator('li', { hasText: 'Apple'});
+    await item.hover();
+    await item.locator('[data-testid="todo-item-toggle"]').click();
+
+    const item1 = page.locator('li', { hasText: 'Cake'});
+    await item1.hover();
+    await item1.locator('[data-testid="todo-item-toggle"]').click();
+
+    await todoPage.buttonCompleted.click();
+    await expect.soft(page.getByText('Apple')).toBeVisible();
+    await expect.soft(page.getByText('Cake')).toBeVisible();
+    await todoPage.buttonClearCompleted.click();
+    await todoPage.buttonAll.click()
+    await expect.soft(page.getByText('Apple')).toHaveCount(0);
+    await expect.soft(page.getByText('Cake')).toHaveCount(0);
+    await expect.soft(page.getByText('Icecream')).toBeVisible();
+
+});
+
+test('mark all tasks as completed using toggle-all', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+    await page.goto('https://todo-app.tallinn-learning.ee/#/');
+    await todoPage.input.fill('task1');
+    await todoPage.input.press('Enter');
+    await todoPage.input.fill('task2');
+    await todoPage.input.press('Enter');
+
+    await todoPage.toggleAll.check();
+
+    await todoPage.buttonCompleted.click();
+    await expect.soft(page.getByText('task1')).toBeVisible();
+    await expect.soft(page.getByText('task2')).toBeVisible();
+});
+
